85c06465cf50c92138d18a69dae7a2e17a06d535
Adam Harwell <flux.adam@gmail.com>
2022-07-17 11:32:40 -0700

    Limit tick names to 32 chars for export

--- ninjalooter/ui/attendance_frame.py
+++ ninjalooter/ui/attendance_frame.py
@@ -396,7 +396,7 @@ class AttendanceDetailWindow(wx.Frame):
 
     def OnClose(self, e: wx.EVT_CLOSE):
         self.item.raidtick = self.raidtick_checkbox.IsChecked()
-        self.item.tick_name = self.name_textbox.GetValue()
+        self.item.tick_name = self.name_textbox.GetValue()[:32]
         self.GetParent().RefreshList()
         utils.store_state()
         self.Destroy()
